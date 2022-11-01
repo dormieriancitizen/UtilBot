5ec19769b5926f2de41abf89148a21316bb2b375
firedestroyer <111535275+dormieriancitizen@users.noreply.github.com>
2022-11-01 16:02:58 +0000

    i added nothing git gud

--- cmd/lag.py
+++ cmd/lag.py
@@ -4,9 +4,9 @@ async def lag(args,message,self):
   if not self.ver:
     return "nu"
   amount = args.pop(0)
-  msg = "🅰️"
+  msg = "🅰"*1990
   await message.channel.send("And thus, it ends.")
   for i in range(int(amount)):
     time.sleep(0.05)
-    await message.channel.send(msg*1000)
+    await message.channel.send(msg)
   return('Pain? Unless your on a phone then not.')
